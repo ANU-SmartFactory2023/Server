b9d5167eaf47f54f450b42d7f0d3e60f831dc5a5
이민성 <tjd5658@naver.com>
2023-11-22 17:40:15 +0900

    파일 추가

---
+++ Server/Controllers/SensorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    public class SensorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
