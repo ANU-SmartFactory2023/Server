4f387d0996ff08ff599ac86ed7c0113c1c0b75aa
이민성 <tjd5658@naver.com>
2023-11-25 09:54:07 +0900

    log 추가

--- Server/Models/LogModel.cs
+++ Server/Models/LogModel.cs
@@ -2,5 +2,15 @@
 {
     public class LogModel
     {
+        public int log_No { get; set; }
+        public string lot_id { get; set; }
+        public int defective { get; set; }
+        public string process_start { get; set; }
+        public string process_end { get; set; }
+        public string process_time { get; set; }
+        public string process1_time { get; set; }
+        public string process2_time { get; set; }
+        public string process3_time { get; set; }
+        public string process4_time { get; set; }
     }
 }
