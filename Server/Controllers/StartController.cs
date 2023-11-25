6a2937d4af9695000fc76ad006f0ac302d98de6e
조은경 <56dmsrud56@naver.com>
2023-11-25 09:57:26 +0900

    add model

---
+++ Server/Controllers/StartController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+using System.Text.Json;
+
+namespace Server.Controllers
+{
+    [Route("pi/[controller]")]
+    [ApiController]
+    public class StartController : Controller
+    {
+        int i;  
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public string GetResponse()
+        {
+            ResponseModel r = new ResponseModel();
+          
+            if(i > 500) // lot 번호 부여 여부 판별
+            {
+                r.msg = "WAIT";
+                r.statusCode = 500;
+            }
+            else
+            {
+                r.msg = "OK";
+                r.statusCode = 200;
+            }
+            
+            return JsonSerializer.Serialize(r);
+        }
+    }
+}
---
+++ Server/Models/LogModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server.Models
+{
+    public class LogModel
+    {
+        public int log_No { get; set; }
+        public String lot_id { get; set; }
+        public int defective { get; set; }
+        public int process_start { get; set; }
+        public int process_end { get; set; }
+        public int process_time { get; set; }
+        public int process1_time { get; set; }
+        public int process2_time { get; set; }
+        public int process3_time { get; set; }
+        public int process4_time { get; set; }
+
+    }
+}
