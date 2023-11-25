68960a37d49f7294013bcdaefa4c3446da79eba0
이민성 <tjd5658@naver.com>
2023-11-25 15:20:30 +0900

    파일 수정

---
+++ Server/Models/MainModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Server.Models
+{
+    public class MainModel
+    {
+        public List<Total_historyModel> Total_historyData { get; set; }
+        public List<Process1Model> process1Data { get; set; }
+        public List<Process2Model> process2Data { get; set; }
+        public List<Process3Model> process3Data { get; set; }
+        public List<Process4Model> process4Data { get; set; }
+    }
+}
